46c1080371ef086c320ee6438c04c3dc2c1c09aa
Hina-Haq <hina-haq@hotmail.com>
2026-04-22 15:32:55 +0200

    Create COMPLETE-lab_sql_Subqueries.sql

---
+++ COMPLETE-lab_sql_Subqueries.sql
@@ -0,0 +1,39 @@
+-- 1 Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
+SELECT COUNT(*) AS copies_in_inventory
+FROM inventory 
+WHERE film_id = (
+	SELECT film_id
+    FROM film
+    WHERE title = 'Hunchback Imposible');
+
+-- 2 Films longer than the average length
+
+SELECT title, length
+FROM film
+WHERE length > (
+    SELECT AVG(length)
+    FROM film
+);
+
+-- 3. Actors who appear in "Alone Trip"
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id IN (
+    SELECT actor_id
+    FROM film_actor
+    WHERE film_id = (
+        SELECT film_id
+        FROM film
+        WHERE title = 'Alone Trip'));
+    
+    
+
+
+
+
+
+
+
+
+
+
